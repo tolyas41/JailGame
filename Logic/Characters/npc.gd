8031602a431e7c2498ee2a86540460367047c9e0
Tolya <zeyatolyas@gmail.com>
2025-06-21 23:17:31 +0300

    + dialogic plugin

--- Logic/Characters/npc.gd
+++ Logic/Characters/npc.gd
@@ -1,5 +1,1 @@
 extends Pawn
-
-#comment from nadya test
-func _on_interact(user) -> void:
-	get_node("../TESTAudio").play()
--- Logic/Interaction/Actors/I_NPC.gd
+++ Logic/Interaction/Actors/I_NPC.gd
@@ -1,5 +1,11 @@
 extends Interactable
 
+@export var Dialog: DialogicTimeline
+
 func interact(user: Node2D) -> void:
 	$TESTAudio.play()
 	#TODO open dialog
+	Dialogic.start(Dialog)
+
+func can_interact() -> bool:
+	return Dialog != null
